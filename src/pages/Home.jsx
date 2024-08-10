ff994f8d0eef75bad95340a404bc82761bcd4326
matias ramos <matiasramos261@gmail.com>
2024-08-10 18:13:19 -0300

    display info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,65 @@
 import { useEffect } from "react";
+import { IconButton, ImageList, ImageListItem, ImageListItemBar, Typography, Box, Tooltip } from "@mui/material";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import InfoIcon from '@mui/icons-material/Info';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { useProducts } from "../hooks/useProducts";
-
 import { Layout } from "../components/Layout";
-import { Box, Button, IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader, Typography } from "@mui/material";
 import { SERVER_URL } from "../helpers/urls";
 
 export function Home() {
-
-    const { products, getProducts } = useProducts()
+    const { products, getProducts } = useProducts();
 
     useEffect(() => {
-        getProducts()
-    }, [])
+        getProducts();
+    }, []);
 
     return (
         <Layout>
             <ImageList sx={{ width: '100%', margin: 0, display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
                 {products.map((product) => (
                     <ImageListItem key={product.id} sx={{ width: '300px' }}>
-                        <img
-                            src={`${SERVER_URL}/${product.images[0].name}.jpg`}
-                            loading="lazy"
-                        />
+                        <Carousel
+                            showArrows={true}
+                            showThumbs={false}
+                            statusFormatter={(currentItem, total) => `${currentItem} de ${total}`}
+                            infiniteLoop
+                        >
+                            {product.images.map((image, index) => (
+                                <img
+                                    key={index}
+                                    src={`${SERVER_URL}/${image.name}.jpg`}
+                                    alt={product.name}
+                                    loading="lazy"
+                                />
+                            ))}
+                        </Carousel>
                         <ImageListItemBar
-                            title={product.name}
-                            subtitle={product.description}
-                            sx={{ backgroundColor: 'rgba(0, 37, 97, 0.8)' }}
+                            title={
+                                <Box>
+                                    <Tooltip title={product.description} placement="top" arrow>
+                                        <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#FFF', cursor: 'pointer' }}>
+                                            {product.name}
+                                        </Typography>
+                                    </Tooltip>
+                                    <Typography variant="subtitle1" sx={{ color: '#FFF' }}>
+                                        ${product.price}
+                                    </Typography>
+                                </Box>
+                            }
+                            position="below"
+                            sx={{ backgroundColor: '#002561', color: '#FFF', pl: 2, pr: 1, alignItems: 'center', gap: 2 }}
                             actionIcon={
                                 <IconButton
-                                    sx={{ color: '#FFF' }}
-                                    aria-label={`info about ${product.name}`}
+                                    sx={{
+                                        color: '#FFF',
+                                        transition: '200ms all',
+                                        ':hover': { backgroundColor: '#FFF', color: '#002561' }
+                                    }}
+                                    aria-label={`add ${product.name} to cart`}
                                 >
-                                    <InfoIcon />
+                                    <AddShoppingCartIcon />
                                 </IconButton>
                             }
                         />
@@ -42,5 +67,5 @@ export function Home() {
                 ))}
             </ImageList>
         </Layout>
-    )
+    );
 }
